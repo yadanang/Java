978090028e9eb00d6ea787becb2dd774877c965b
printf（“HelloLJinY”） <2192097421@qq.com>
2024-07-23 23:43:51 +0800

    test

--- Test.java
+++ Test.java
@@ -46,7 +46,51 @@ public class Test {
         System.out.println(11.5 % 2.0);//特例*/
 //        System.out.println();
 
-        
+       /* System.out.println(Long.MAX_VALUE);
+        long a = 9223372036854775808L;
+        System.out.println(a);
+*/
+        /*int a = 1;
+        int b = 2;
+        System.out.println(a/b);*/
+//        int a = 1;
+//        double b = 2;
+//        System.out.println(a/b);//发生整形提升
 
+       /* float a = 6.66f;
+        float b = 1;//发生类型转换
+        System.out.println(b);*/
+
+//        int num = 10;
+//        String str1 = String.valueOf(num);
+//        int num2 = Integer.parseInt(str1);
+
+//        System.out.printf("%s", num);
+
+
+//        int a=10;
+//        long b = 100;
+////        System.out.println(a += b);
+//        a =(int) (a + b);
+//        System.out.println(a);
+
+//        System.out.println(10 > 20 & 10 / 0 == 0);
+
+//        System.out.println(90 | 20);
+       /* int a = 1;
+        int b = 1;
+        System.out.printf("%d\n", ~a);*/
+
+        /*int a = 50;
+        System.out.println(a >> 1);
+        System.out.println(a >>> 1);*/
+
+        //求a和b的平均值
+//        int a = 10;
+//        int b = 20;
+//        int d =  a + ((b-a)>>1);
+//        System.out.println(d);
+
+        
     }
 }
