ef8fdd0bec205020a088a0110624dae33cf40873
printf（“HelloLJinY”） <2192097421@qq.com>
2024-07-23 17:13:40 +0800

    Test.java

---
+++ Test.java
@@ -0,0 +1,52 @@
+public class Test {
+    public static void main(String[] args) {
+
+/*
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);*/
+
+
+       /* float a = 6.6666888888f;
+        System.out.println(a);*/
+
+       /* char c1 = '你';
+        System.out.println(c1);
+
+        System.out.println(Character.MAX_VALUE);
+        System.out.println(Character.MIN_VALUE);*/
+
+//        boolean a = false;
+
+       /* System.out.println(5 / 2);//2
+        System.out.println((float)5 / 2);//2.5
+
+        System.out.println(5 / (float)2);//2.5
+        System.out.println((float)(5 / 2));//2.0*/
+
+        /*System.out.println(10 % 3);//1
+        System.out.println(10 % -3);//1
+        System.out.println(-10 % 3);//-1
+        System.out.println(-10 % -3);//-1
+
+        System.out.println(11.5 % 2.0);//特例*/
+//        System.out.println();
+
+        
+
+    }
+}
